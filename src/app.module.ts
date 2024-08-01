7934896ea57c31e18df72d999a46b7ece7a51b32
gnauqoa <quanglng2022@gmail.com>
2024-08-01 09:26:48 +0700

    Init nest config.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'quang',
-      password: 'quang',
-      database: 'weather_whiz_service',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Don't use in production - may cause data loss
-    }),
     UserModule,
     AuthModule,
+    CommonModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
