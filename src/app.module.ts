7f988d9a4d86fa949f7650f0712af34217287408
gnauqoa <quanglng2022@gmail.com>
2024-08-01 07:51:32 +0700

    Format project.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'quang',
+      password: 'quang',
+      database: 'T',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Don't use in production - may cause data loss
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
