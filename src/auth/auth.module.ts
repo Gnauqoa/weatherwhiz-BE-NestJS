4d592d243405c546eb5bcffda11b49185d30c234
gnauqoa <quanglng2022@gmail.com>
2024-08-02 09:36:24 +0700

    Fix missing service error.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { LoginService } from './login/login.service';
-import { RegisterService } from './register/register.service';
 import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [LoginService, RegisterService],
+  providers: [],
   controllers: [AuthController],
 })
 export class AuthModule {}
