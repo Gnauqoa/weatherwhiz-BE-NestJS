9cd1a38ad295861d66f0a85a9949065940e9c5e6
gnauqoa <quanglng2022@gmail.com>
2024-08-02 09:57:56 +0700

    Init user auth with JWT.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  Dependencies,
+  UnauthorizedException,
+} from '@nestjs/common';
 
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/user/user.service';
+
+@Dependencies(UsersService, JwtService)
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signIn(username, pass) {
+    const user = await this.usersService.findByAccount(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
