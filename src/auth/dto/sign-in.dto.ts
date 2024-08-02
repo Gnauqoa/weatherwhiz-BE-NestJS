bd1f8b744e38300fbd6fa6334654216cf7062b05
gnauqoa <quanglng2022@gmail.com>
2024-08-02 11:47:46 +0700

    Add desc to SignInDto.

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -2,7 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class SignInDto {
-  @ApiProperty({ example: 'quanglng2022@gmail.com' })
+  @ApiProperty({
+    example: 'quanglng2022@gmail.com',
+    description: 'Email or username',
+  })
   @IsString()
   @IsNotEmpty()
   account: string;
